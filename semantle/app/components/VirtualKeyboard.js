40fb1ecb96d351306063666c31209c4a763c8275
Nathan Stone <34358872+KeystoneScience@users.noreply.github.com>
2022-03-21 12:15:52 -0600

    adjusted on screen keyboard

--- semantle/app/components/VirtualKeyboard.js
+++ semantle/app/components/VirtualKeyboard.js
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import { BlurView } from "expo-blur";
 import Constants from "expo-constants";
+import colors from "../configs/colors";
 
 var keys = [
   ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
@@ -20,7 +21,7 @@ var keys = [
 
 export default function VirtualKeyboard({ onKey, onBackspace, onEnter }) {
   return (
-    <BlurView
+    <View
       intensity={100}
       style={{
         position: "absolute",
@@ -28,7 +29,7 @@ export default function VirtualKeyboard({ onKey, onBackspace, onEnter }) {
         bottom: 60,
 
         alignSelf: "center",
-        backgroundColor: "rgba(0,0,0,0.5)",
+        // backgroundColor: "rgba(0,0,0,0.5)",
         paddingBottom: 20,
       }}
     >
@@ -36,7 +37,7 @@ export default function VirtualKeyboard({ onKey, onBackspace, onEnter }) {
         <View key={i} style={styles.row}>
           {row.map((key, j) => (
             <TouchableOpacity
-                
+              style={[styles.key]}
               onPress={() => {
                 if (key === "DEL") {
                   onBackspace();
@@ -47,20 +48,14 @@ export default function VirtualKeyboard({ onKey, onBackspace, onEnter }) {
                 }
               }}
             >
-              <Text
-                key={j}
-                style={[
-                  styles.key,
-                  { width: key === "ENTER" || key === "DEL" ? null : null },
-                ]}
-              >
+              <Text key={j} style={[styles.keyText]}>
                 {key}
               </Text>
             </TouchableOpacity>
           ))}
         </View>
       ))}
-    </BlurView>
+    </View>
   );
 }
 
@@ -72,9 +67,20 @@ const styles = StyleSheet.create({
   },
   key: {
     fontSize: 20,
-    // backgroundColor: "rgba(58, 12, 163, .1)",
-    padding: 5,
+    backgroundColor: "rgba(58, 12, 163, .6)",
+    minWidth: 30,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 2,
+    paddingRight: 2,
+    borderRadius: 12,
     // width: 30,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  keyText: {
+    fontSize: 20,
+    color: colors.colors.white,
     textAlign: "center",
     fontWeight: "bold",
   },
