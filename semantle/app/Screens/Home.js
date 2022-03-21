1cc2e651fa95d94744438c102823689de0288874
Nathan Stone <34358872+KeystoneScience@users.noreply.github.com>
2022-03-21 02:43:27 -0600

    added in colors and header

--- semantle/app/Screens/Home.js
+++ semantle/app/Screens/Home.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 import GuessList from "../components/GuessList";
 import GuessListHeader from "../components/GuessListHeader";
 import semantle from "../functions/semantle";
+import Header from "../components/Header";
 const guessArray = [
   {
     index: 1,
@@ -37,43 +38,49 @@ function Home({ navigation, route }) {
   //   semantleGame.submit("test", "home");
   // }, []);
   return (
-    <SafeAreaView
-      style={{
-        width: "100%",
-        height: "100%",
-        backgroundColor: "rgba(58, 12, 163, .1)",
-      }}
-    >
-      <Text onPress={() => navigation.navigate("Drawer")} style={styles.title}>
-        Semantle
-      </Text>
-      <Text style={styles.subtitle}>can you guess the word?</Text>
-      <MainInput
-        onSubmit={(value) => {
-          semantleGame.submit(value, "test");
-        }}
-      />
-      <View
+    <View>
+      <Header navigation={navigation} route={route} />
+      <SafeAreaView
         style={{
-          borderBottomEndRadius: 5,
-          borderBottomStartRadius: 5,
-          borderTopLeftRadius: 5,
-          borderTopRightRadius: 5,
-          overflow: "hidden",
-          width: "95%",
-          alignSelf: "center",
-          height: "auto",
+          width: "100%",
+          height: "100%",
           backgroundColor: "rgba(58, 12, 163, .1)",
         }}
       >
-        <GuessListHeader />
-        <ScrollView>
-          {semantleGame.guesses.map((obj, index) => (
-            <GuessList key={index} {...obj} />
-          ))}
-        </ScrollView>
-      </View>
-    </SafeAreaView>
+        <Text
+          onPress={() => navigation.navigate("Drawer")}
+          style={styles.title}
+        >
+          Semantle
+        </Text>
+        <Text style={styles.subtitle}>can you guess the word?</Text>
+        <MainInput
+          onSubmit={(value) => {
+            semantleGame.submit(value, "test");
+          }}
+        />
+        <View
+          style={{
+            borderBottomEndRadius: 5,
+            borderBottomStartRadius: 5,
+            borderTopLeftRadius: 5,
+            borderTopRightRadius: 5,
+            overflow: "hidden",
+            width: "95%",
+            alignSelf: "center",
+            height: "auto",
+            backgroundColor: "rgba(58, 12, 163, .1)",
+          }}
+        >
+          <GuessListHeader />
+          <ScrollView>
+            {semantleGame.guesses.map((obj, index) => (
+              <GuessList key={index} {...obj} />
+            ))}
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 }
 
---
+++ semantle/app/components/Header.js
@@ -0,0 +1,41 @@
+import React, { useRef, useState, useEffect } from "react";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import Constants from "expo-constants";
+import colors from "../configs/colors";
+
+export default function Header() {
+  return (
+    <View style={{ zIndex: 1 }}>
+      <View
+        style={{
+          backgroundColor: colors.colors.grooveColorPallet[2],
+          height: Constants.statusBarHeight,
+        }}
+      />
+      <View
+        style={{
+          alignItems: "center",
+          backgroundColor: colors.colors.grooveColorPallet[2],
+          height: 40,
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          width: "100%",
+          zIndex: 1,
+        }}
+      >
+        <Text
+          style={{
+            color: colors.colors.white,
+            fontSize: 21,
+            fontWeight: "800",
+            marginLeft: 10,
+            textAlign: "center",
+            marginRight: 10,
+          }}
+        >
+          Semantle
+        </Text>
+      </View>
+    </View>
+  );
+}
