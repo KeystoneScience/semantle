b0e95818373f6d1d5bd32461d62ded7cad02f502
Nathan Stone <34358872+KeystoneScience@users.noreply.github.com>
2022-03-22 09:41:56 -0600

    added in ranks

--- semantle/app/components/AppText.js
+++ semantle/app/components/AppText.js
@@ -2,12 +2,28 @@ import React from "react";
 import AppLoading from "expo-app-loading";
 import { useFonts } from "expo-font";
 import { StyleSheet, Text, View } from "react-native";
+const fonts = [
+  "BalooBhaina2_400Regular",
+  "BalooBhaina2_500Medium",
+  "BalooBhaina2_600SemiBold",
+  "BalooBhaina2_700Bold",
+  "BalooBhaina2_800ExtraBold",
+];
 function AppText(props) {
   return (
     <Text
       style={[
         {
-          fontFamily: "BalooBhaina2_600SemiBold",
+          fontFamily:
+            props.fontWeight < 450
+              ? fonts[0]
+              : props.fontWeight < 550
+              ? fonts[1]
+              : props.fontWeight < 650
+              ? fonts[2]
+              : props.fontWeight < 750
+              ? fonts[3]
+              : fonts[4],
         },
         props.style,
       ]}
